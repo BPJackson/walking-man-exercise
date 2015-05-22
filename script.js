ec815e3a020fe380038aa68081fa2ac91f0f2ef1
brianpjackson88@gmail.com <brianpjackson88@gmail.com>
2015-05-22 08:46:47 -0600

    made the man walk

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ var speed = 0;
 
 var setPosition = function () {
   if (position >=1100){
-    speed = -500;
+    speed = -5;
     img.style.transform = 'rotateY(180deg)';
   }
 
   if (position <= 0){
-    speed = 500;
-    img.style.transform = '';
+    speed = 5;
+    img.style.transform = 'rotateX(180deg)';
   }
   position += speed;
   img.style.marginLeft = position + 'px';
@@ -22,52 +22,16 @@ var setPosition = function () {
 
 
 
-//
-// function move (id,speed){
-//   var img = document.getElementById('img');
-//   var right = 0;
-//
-//     right += 2;
-//     img.style.left = right + 'px';
-//
-//     var moveRight = setTimeout(function() {
-//       move(id,speed);
-//     }, 2);
-// }
-// move('',2);
-//
-// // function moveRight(id, speed) {
-// //   var img = document.getElementById('img');
-// //     var right = parseInt(img.style.left)||0;
-// //
-// //   right += speed;  // move
-// //     img.style.left = right + "px";
-// //
-// //     var move = setInterval(function() {
-// //         moveRight(id, speed);
-// //     }, 50);
-// //
-// // }
-// // moveRight('',2);
-//
-//
-//
-//
-//
-// // function moveRight(){
-// //   var margin = img.style.marginLeft
-// //   if (margin<1000){
-// //     img.style.left = parseInt(img.style.left+100)+'px';
-// //   }
-// //
-// //   }
-// // }
-// //
-// //
-// // setInterval(move(), 20);
-//
-// // img.addEventListener ('click', function() {
-// //  move();
-// //   //img.style.left = parseInt("100px");
-// //
-// // });
+function moveRight(){
+  var margin = img.style.marginLeft
+  if (margin<1000){
+    img.style.left = parseInt(img.style.left+100)+'px';
+  }
+
+  }
+
+img.addEventListener ('click', function() {
+ move();
+  img.style.left = parseInt("100px");
+
+});
